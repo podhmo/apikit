53c33b08a41878c22cfb7296eed7744c1c582b49
podhmo <ababjam61+github@gmail.com>
2021-09-01 22:03:11 +0900

    feat: add difftest

---
+++ difftest/difftest.go
@@ -0,0 +1,74 @@
+package difftest
+
+import (
+	"encoding/json"
+	"strings"
+	"testing"
+	"unsafe"
+
+	"github.com/gookit/color"
+	"github.com/shibukawa/cdiff"
+)
+
+// TODO: write to files
+func AssertGotAndWantBytes(t *testing.T, got interface{}, want []byte) {
+	t.Helper()
+
+	// normalize
+	var wantString, gotString string
+	{
+		b, err := json.Marshal(got)
+		if err != nil {
+			t.Fatalf("marshal for got, %+v, %+v", got, err)
+		}
+		var v interface{}
+		if err := json.Unmarshal(b, &v); err != nil {
+			t.Fatalf("marshal,unmarshal for got, %+v, %+v", got, err)
+		}
+		b2, err := json.MarshalIndent(v, "", "  ")
+		if err != nil {
+			t.Fatalf("marshal,unmarshal,marshal for got, %+v, %+v", got, err)
+		}
+		gotString = strings.TrimSpace(*(*string)(unsafe.Pointer(&b2)))
+	}
+	{
+		var v interface{}
+		if err := json.Unmarshal(want, &v); err != nil {
+			t.Fatalf("unmarshal for want, %+v, %+v", string(want), err)
+		}
+		b2, err := json.MarshalIndent(v, "", "  ")
+		if err != nil {
+			t.Fatalf("unmarshal,marshal for want, %+v, %+v", string(want), err)
+		}
+		wantString = strings.TrimSpace(*(*string)(unsafe.Pointer(&b2)))
+	}
+
+	diff := cdiff.Diff(gotString, wantString, cdiff.WordByWord)
+	output := diff.UnifiedWithGooKitColor("got", "want", 3, cdiff.GooKitColorTheme)
+
+	// first 4lines is header
+	if len(strings.SplitAfterN(output, "\n", 5)) >= 5 {
+		t.Errorf(color.Sprintf(output))
+		if testing.Verbose() {
+			t.Logf("want\n%s", wantString)
+			t.Logf("but got\n%s", gotString)
+		}
+	}
+}
+
+func AssertGotAndWantString(t *testing.T, got interface{}, want string) {
+	t.Helper()
+	AssertGotAndWantBytes(t, got, []byte(want))
+}
+
+func LogDiffGotStringAndWantString(t *testing.T, gotString, wantString string) {
+	diff := cdiff.Diff(gotString, wantString, cdiff.WordByWord)
+	output := diff.UnifiedWithGooKitColor("got", "want", 3, cdiff.GooKitColorTheme)
+
+	// first 4lines is header
+	t.Errorf(color.Sprintf(output))
+	if testing.Verbose() {
+		t.Logf("want\n%s", wantString)
+		t.Logf("but got\n%s", gotString)
+	}
+}
