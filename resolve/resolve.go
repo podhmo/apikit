c71fbf8f2a5eed8a6285031c24d83ab3c35abece
podhmo <ababjam61+github@gmail.com>
2021-09-11 23:11:25 +0900

    refactor: to extractDef

--- resolve/resolve.go
+++ resolve/resolve.go
@@ -20,89 +20,12 @@ func NewResolver() *Resolver {
 		RevisitArglist: true,
 	}
 	return &Resolver{
-		extractor: e,
-		universe:  tinypkg.NewUniverse(),
-	}
-}
-
-func detectKind(s reflectshape.Shape) Kind {
-	if s.GetLv() > 0 {
-		// TODO: if the pointer of primitive passed, treated as optional value? (not yet)
-		return KindComponent
-	}
-
-	switch s := s.(type) {
-	case reflectshape.Primitive:
-		return KindPrimitive
-	case reflectshape.Interface:
-		if s.GetFullName() == "context.Context" {
-			return KindIgnored
-		} else {
-			return KindComponent
-		}
-	case reflectshape.Struct:
-		return KindData
-	case reflectshape.Container: // slice, map
-		return KindUnsupported
-	case reflectshape.Function:
-		return KindComponent
-	default:
-		return KindUnsupported
+		extractor:    e,
+		universe:     tinypkg.NewUniverse(),
+		symbolsCache: map[reflectshape.Shape][]*symbolCacheItem{},
 	}
 }
 
 func (r *Resolver) Resolve(fn interface{}) *Def {
-	sfn := r.extractor.Extract(fn).(reflectshape.Function)
-	pkg := r.universe.NewPackage(sfn.Package, "")
-	args := make([]Item, 0, len(sfn.Params.Keys))
-	returns := make([]Item, 0, len(sfn.Returns.Keys))
-
-	for i, name := range sfn.Params.Keys {
-		s := sfn.Params.Values[i]
-		kind := detectKind(s)
-		args = append(args, Item{
-			Kind:  kind,
-			Name:  name,
-			Shape: s,
-		})
-	}
-	for i, name := range sfn.Returns.Keys {
-		s := sfn.Returns.Values[i]
-		kind := detectKind(s)
-		returns = append(returns, Item{
-			Kind:  kind,
-			Name:  name,
-			Shape: s,
-		})
-	}
-
-	return &Def{
-		Symbol:  pkg.NewSymbol(sfn.Name),
-		Shape:   sfn,
-		Args:    args,
-		Returns: returns,
-	}
+	return ExtractDef(r.universe, r.extractor, fn)
 }
-
-type Def struct {
-	*tinypkg.Symbol
-	Shape   reflectshape.Function
-	Args    []Item
-	Returns []Item
-}
-
-type Item struct {
-	Kind  Kind
-	Name  string
-	Shape reflectshape.Shape
-}
-
-type Kind string
-
-const (
-	KindComponent   Kind = "component"   // pointer, function, interface
-	KindData        Kind = "data"        // struct
-	KindIgnored     Kind = "ignoerd"     // context.Context
-	KindPrimitive   Kind = "primitve"    // string, int, ...
-	KindUnsupported Kind = "unsupported" // slice, map
-)
