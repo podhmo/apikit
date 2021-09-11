05652a7ca67e90739ec912d6f87630fc02bfe901
podhmo <ababjam61+github@gmail.com>
2021-09-12 02:16:31 +0900

    refactor: separate files

---
+++ translate/code.go
@@ -0,0 +1,40 @@
+package translate
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/podhmo/apikit/tinypkg"
+)
+
+type Code struct {
+	Name string
+	Here *tinypkg.Package
+
+	ImportPackages func() ([]*tinypkg.ImportedPackage, error)
+	EmitCode       func(w io.Writer) error
+
+	EmitFunc
+}
+
+var ErrNoImports = fmt.Errorf("no imports")
+
+func (c *Code) EmitImports(w io.Writer) error {
+	impkgs, err := c.ImportPackages()
+	if err != nil {
+		return err
+	}
+	if len(impkgs) == 0 {
+		return ErrNoImports
+	}
+
+	io.WriteString(w, "import (\n")
+	for _, impkg := range impkgs {
+		io.WriteString(w, "\t")
+		impkg.Emit(w)
+	}
+	io.WriteString(w, ")\n")
+	return nil
+}
+
+var _ Emitter = &Code{}
