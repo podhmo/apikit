767be58c7c812e21121bd1e0ddf8a4dc48b04117
podhmo <ababjam61+github@gmail.com>
2021-09-11 23:18:43 +0900

    feat: add ResolveSymbol method

--- resolve/resolve.go
+++ resolve/resolve.go
@@ -2,6 +2,7 @@ package resolve
 
 import (
 	"reflect"
+	"sync"
 
 	"github.com/podhmo/apikit/tinypkg"
 	reflectshape "github.com/podhmo/reflect-shape"
@@ -11,6 +12,9 @@ import (
 type Resolver struct {
 	extractor *reflectshape.Extractor
 	universe  *tinypkg.Universe
+
+	mu           sync.RWMutex
+	symbolsCache map[reflectshape.Shape][]*symbolCacheItem
 }
 
 func NewResolver() *Resolver {
@@ -28,4 +32,33 @@ func NewResolver() *Resolver {
 
 func (r *Resolver) Resolve(fn interface{}) *Def {
 	return ExtractDef(r.universe, r.extractor, fn)
+}
+
+type symbolCacheItem struct {
+	Here     *tinypkg.Package
+	Shape    reflectshape.Shape
+	Symboler tinypkg.Symboler
+}
+
+func (r *Resolver) ResolveSymbol(here *tinypkg.Package, s reflectshape.Shape) tinypkg.Symboler {
+	defer r.mu.Unlock()
+	r.mu.RLock()
+	cached, ok := r.symbolsCache[s]
+	r.mu.RUnlock()
+	if ok {
+		for _, item := range cached {
+			if item.Here == here {
+				return item.Symboler
+			}
+		}
+	}
+	symboler := ExtractSymbol(here, s)
+	r.mu.Lock()
+	r.symbolsCache[s] = append(cached, &symbolCacheItem{
+		Here:     here,
+		Shape:    s,
+		Symboler: symboler,
+	})
+	r.mu.Unlock()
+	return symboler
 }
