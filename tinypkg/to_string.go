03d2ee3cf69b8b62e9c27b48df9ee25c1a86a32f
podhmo <ababjam61+github@gmail.com>
2021-09-12 12:55:23 +0900

    refactor: use tinypkg.WriteFunc()

--- tinypkg/to_string.go
+++ tinypkg/to_string.go
@@ -5,6 +5,13 @@ import (
 	"strings"
 )
 
+func ToImportPackageString(ip *ImportedPackage) string {
+	if ip.qualifier != "" {
+		return fmt.Sprintf("%s %q", ip.qualifier, ip.pkg.Path)
+	}
+	return fmt.Sprintf("%q", ip.pkg.Path)
+}
+
 func ToRelativeTypeString(here *Package, Node Node) string {
 	switch x := Node.(type) {
 	case *Var:
---
+++ tinypkg/write_something.go
@@ -0,0 +1,33 @@
+package tinypkg
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+func WriteFunc(w io.Writer, name string, f *Func, body func() error) error {
+	args := make([]string, 0, len(f.Params))
+	for _, x := range f.Params {
+		args = append(args, x.String())
+	}
+
+	returns := make([]string, 0, len(f.Returns))
+	for _, x := range f.Returns {
+		returns = append(returns, x.String())
+	}
+
+	// func <name>(<args>...) (<returns>) {
+	// ...
+	// }
+	defer fmt.Fprintln(w, "}")
+	switch len(returns) {
+	case 0:
+		fmt.Fprintf(w, "func %s(%s) {\n", name, strings.Join(args, ", "))
+	case 1:
+		fmt.Fprintf(w, "func %s(%s) %s {\n", name, strings.Join(args, ", "), returns[0])
+	default:
+		fmt.Fprintf(w, "func %s(%s) (%s) {\n", name, strings.Join(args, ", "), strings.Join(returns, ", "))
+	}
+	return body()
+}
