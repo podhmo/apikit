5785de082b0d26cca4967999e6c6ea36e023f3fd
podhmo <ababjam61+github@gmail.com>
2021-09-14 01:21:24 +0900

    chore: update examples

--- _examples/00simple/design/action.go
+++ _examples/00simple/design/action.go
@@ -2,7 +2,12 @@ package design
 
 type DB struct{}
 type User struct{}
+type Messenger struct{}
 
 func ListUser(db *DB) ([]*User, error) {
 	return nil, nil
+}
+
+func SendMessage(m *Messenger, title string) error {
+	return nil
 }
