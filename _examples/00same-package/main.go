85caa768715ed432707ec72ceb13e1cc1e24fe19
podhmo <ababjam61+github@gmail.com>
2021-09-16 02:18:19 +0900

    chore: update examples

--- _examples/00same-package/main.go
+++ _examples/00same-package/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"log"
 
 	"m/00same-package/design"
-	"m/fileutil"
 
+	"github.com/podhmo/apikit/pkg/emitfile"
 	"github.com/podhmo/apikit/pkg/tinypkg"
 	"github.com/podhmo/apikit/resolve"
 	"github.com/podhmo/apikit/translate"
@@ -19,43 +18,37 @@ func main() {
 	}
 }
 
-func run() error {
-	resolver := resolve.NewResolver()
+func run() (retErr error) {
+	rootdir := "./00same-package"
+	emitter := emitfile.New(rootdir)
+	defer func() {
+		if err := emitter.Emit(); err != nil {
+			retErr = err
+		}
+	}()
 
+	resolver := resolve.NewResolver()
 	translator := translate.NewTranslator(resolver, design.ListUser)
-	translator.Override("m", func() (*design.Messenger, error) { return nil, nil })
 	dst := tinypkg.NewPackage("m/00same-package/runner", "")
 
 	{
 		here := dst
 		code := translator.TranslateToInterface(here, "Component")
-		var buf bytes.Buffer
-		if err := code.Emit(&buf, code); err != nil {
-			return nil
-		}
-		fileutil.WriteOrCreateFile("./00same-package/runner/component.go", buf.Bytes())
+		emitter.Register("/runner/component.go", code)
 	}
-
-	// TODO: detect provider name after emit code
 	{
 		pkg := dst
 		def := resolver.Def(design.ListUser)
 		code := translator.TranslateToRunner(pkg, def, "", nil)
-		var buf bytes.Buffer
-		if err := code.Emit(&buf, code); err != nil {
-			return nil
-		}
-		fileutil.WriteOrCreateFile(fmt.Sprintf("./00same-package/runner/%s.go", def.Name), buf.Bytes())
+		emitter.Register(fmt.Sprintf("/runner/%s.go", def.Name), code)
 	}
 	{
 		pkg := dst
 		def := resolver.Def(design.SendMessage)
 		code := translator.TranslateToRunner(pkg, def, "", nil)
-		var buf bytes.Buffer
-		if err := code.Emit(&buf, code); err != nil {
-			return nil
-		}
-		fileutil.WriteOrCreateFile(fmt.Sprintf("./00same-package/runner/%s.go", def.Name), buf.Bytes())
+		emitter.Register(fmt.Sprintf("/runner/%s.go", def.Name), code)
 	}
+
+	translator.Override("m", func() (*design.Messenger, error) { return nil, nil })
 	return nil
 }
