a254b30ce53c63d71486c1135bba04f3e478f3ea
podhmo <ababjam61+github@gmail.com>
2021-09-20 05:44:02 +0900

    refactor: rename methods and fields

--- pkg/emitgo/emitter.go
+++ pkg/emitgo/emitter.go
@@ -10,34 +10,38 @@ import (
 )
 
 type Emitter struct {
-	*emitfile.Executor
-	RootPkg *tinypkg.Package
-	RootDir string
+	FileEmitter *emitfile.Executor
+	RootPkg     *tinypkg.Package
+	RootDir     string
 }
 
-func NewEmitterFromRelativePath(fn interface{}, relative string) *Emitter {
+func NewFromRelativePath(fn interface{}, relative string) *Emitter {
 	rootdir := filepath.Join(DefinedDir(fn), relative)
 	rootpkg := tinypkg.NewPackage(path.Join(PackagePath(fn), relative), "")
 	return New(rootdir, rootpkg)
 }
 func New(rootdir string, rootpkg *tinypkg.Package) *Emitter {
 	emitter := &Emitter{
-		Executor: emitfile.New(rootdir),
-		RootPkg:  rootpkg,
-		RootDir:  rootdir,
+		FileEmitter: emitfile.New(rootdir),
+		RootPkg:     rootpkg,
+		RootDir:     rootdir,
 	}
-	emitter.Executor.PathResolver.AddRoot("/"+rootpkg.Path, rootdir)
+	emitter.FileEmitter.PathResolver.AddRoot("/"+rootpkg.Path, rootdir)
 	return emitter
 }
 
 func (e *Emitter) EmitWith(errptr *error) {
-	if err := e.Emit(); err != nil {
+	if err := e.FileEmitter.Emit(); err != nil {
 		*errptr = err
 	}
 }
+func (e *Emitter) Emit() error {
+	return e.FileEmitter.Emit()
+}
+
 func (e *Emitter) Register(pkg *tinypkg.Package, name string, target emitfile.Emitter) *emitfile.EmitAction {
 	if !strings.HasSuffix(name, ".go") {
 		name = name + ".go"
 	}
-	return e.Executor.Register("/"+path.Join(pkg.Path, name), target)
+	return e.FileEmitter.Register("/"+path.Join(pkg.Path, name), target)
 }
