bb0811e6f17183ede476e2fde9588302d2ecab14
podhmo <ababjam61+github@gmail.com>
2021-09-20 17:26:19 +0900

    feat: handling return type

--- translate/to_composed.go
+++ translate/to_composed.go
@@ -19,7 +19,6 @@ func writeComposed(
 	// TODO: external
 	var args []*tinypkg.Var
 	var returns []*tinypkg.Var
-
 	symbols := map[reflectshape.Identity]*tinypkg.Symbol{}
 	i := 0
 	for _, p := range providers {
@@ -27,27 +26,43 @@ func writeComposed(
 		symbols[k] = here.NewSymbol(fmt.Sprintf("v%d", i))
 		i++
 	}
+	for _, p := range providers {
+		for _, x := range p.Args {
+			k := x.Shape.GetIdentity()
+			_, ok := symbols[k]
+			if !ok {
+				args = append(args, &tinypkg.Var{Name: x.Name, Node: resolver.Symbol(here, x.Shape)})
+				symbols[k] = here.NewSymbol(x.Name)
+			}
+		}
+	}
 
+	{
+		sym := resolver.Symbol(here, providers[len(providers)-1].Shape)
+		f, ok := sym.(*tinypkg.Func)
+		if !ok {
+			return fmt.Errorf("unexpected return type providers[%d] (%s)", len(providers)-1, sym)
+		}
+		returns = f.Returns
+	}
 	return tinypkg.WriteFunc(w, name, &tinypkg.Func{Args: args, Returns: returns},
 		func() error {
+			var retK reflectshape.Identity
 			for _, p := range providers {
-				retK := p.Returns[0].Shape.GetIdentity()
+				retK = p.Returns[0].Shape.GetIdentity()
 				lhs := symbols[retK] // TODO: handling, multiple values
 
 				args := make([]string, 0, len(p.Args))
 				for _, x := range p.Args {
 					k := x.Shape.GetIdentity()
-					v, ok := symbols[k]
-					if !ok {
-						// as external args
-						v = here.NewSymbol(x.Name)
-					}
+					v := symbols[k]
 					args = append(args, v.Name)
 				}
 				rhs := fmt.Sprintf("%s(%s)", tinypkg.ToRelativeTypeString(here, p.Symbol), strings.Join(args, ", "))
 				fmt.Fprintf(w, "\t%s := %s\n", lhs, rhs)
 				i++
 			}
+			fmt.Fprintf(w, "\treturn %s\n", symbols[retK].Name)
 			return nil
 		})
 }
