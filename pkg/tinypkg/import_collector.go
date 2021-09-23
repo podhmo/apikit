01f4d33533368f59f917840bd2d9cf2d04f3ab65
podhmo <ababjam61+github@gmail.com>
2021-09-23 10:05:56 +0900

    feat: add ImportCollector

---
+++ pkg/tinypkg/import_collector.go
@@ -0,0 +1,31 @@
+package tinypkg
+
+func NewImportCollector(here *Package) *ImportCollector {
+	return &ImportCollector{
+		Here: here,
+		seen: map[*Package]bool{},
+	}
+}
+
+type ImportCollector struct {
+	Here    *Package
+	Imports []*ImportedPackage
+	seen    map[*Package]bool
+}
+
+func (c *ImportCollector) Collect(sym *Symbol) error {
+	seen := c.seen
+	here := c.Here
+	if sym.Package.Path == "" {
+		return nil // bultins type (e.g. string, bool, ...)
+	}
+	if _, ok := seen[sym.Package]; ok {
+		return nil
+	}
+	seen[sym.Package] = true
+	if here == sym.Package {
+		return nil
+	}
+	c.Imports = append(c.Imports, here.Import(sym.Package))
+	return nil
+}
