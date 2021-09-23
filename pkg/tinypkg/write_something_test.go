83347caf1dc1d9b7598a624e51ca9a362bc72510
podhmo <ababjam61+github@gmail.com>
2021-09-23 09:30:58 +0900

    test: add tests (write func)

--- pkg/tinypkg/write_something_test.go
+++ pkg/tinypkg/write_something_test.go
@@ -1,11 +1,68 @@
 package tinypkg
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
 	"github.com/podhmo/apikit/pkg/difftest"
 )
+
+func TestWriteFunc(t *testing.T) {
+	pkg := NewPackage("m/foo", "")
+	main := NewPackage("main", "")
+
+	fn := pkg.NewFunc(
+		"DoSomething",
+		[]*Var{{Name: "ctx", Node: NewPackage("context", "").NewSymbol("Context")}},
+		[]*Var{
+			{Node: &Pointer{Lv: 1, V: pkg.NewSymbol("Foo")}},
+			{Node: NewSymbol("error")},
+		},
+	)
+
+	cases := []struct {
+		msg   string
+		input *Func
+		here  *Package
+		want  string
+	}{
+		{
+			msg:   "same package",
+			input: fn,
+			here:  pkg,
+			want: `
+func DoSomething(ctx context.Context) (*Foo, error) {
+	return nil, nil
+}`,
+		},
+		{
+			msg:   "another package",
+			input: fn,
+			here:  main,
+			want: `
+func DoSomething(ctx context.Context) (*foo.Foo, error) {
+	return nil, nil
+}`,
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.msg, func(t *testing.T) {
+			var buf strings.Builder
+			if err := WriteFunc(&buf, c.here, "", fn, func() error {
+				fmt.Fprintln(&buf, "\treturn nil, nil")
+				return nil
+			}); err != nil {
+				t.Fatalf("unexpected error %+v", err)
+			}
+			if want, got := strings.TrimSpace(c.want), strings.TrimSpace(buf.String()); want != got {
+				difftest.LogDiffGotStringAndWantString(t, got, want)
+			}
+		})
+	}
+}
 
 func TestWriteInterface(t *testing.T) {
 	pkg := NewPackage("net/http", "")
