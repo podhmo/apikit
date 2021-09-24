051be064c981951a695e3f20604ea56284a4d9e1
podhmo <ababjam61+github@gmail.com>
2021-09-25 01:47:10 +0900

    Write binding, in tinypkg (#74)
    * refactor: fix for lint
    
    * feat: initial implementation of binding
    
    * test: start writing test
    
    * fix: more tests, and adjustment
    
    * feat: NewBinding()
    
    * doc: add comments
    
    * feat: add provider alias

--- pkg/tinypkg/tinypkg_test.go
+++ pkg/tinypkg/tinypkg_test.go
@@ -1,7 +1,6 @@
 package tinypkg
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -16,7 +15,7 @@ func TestSymbol(t *testing.T) {
 		Bar := main.NewSymbol("Bar")
 
 		t.Run("imported package", func(t *testing.T) {
-			got := fmt.Sprintf("%s", mainFoo)
+			got := mainFoo.String()
 			want := "xxx.Foo"
 			if want != got {
 				t.Errorf("\nwant:\n\t%q\nbut got:\n\t%q", want, got)
@@ -24,14 +23,14 @@ func TestSymbol(t *testing.T) {
 		})
 		t.Run("qualified imported package", func(t *testing.T) {
 			mainFoo := main.ImportAs(xxx, "x").Lookup(Foo)
-			got := fmt.Sprintf("%s", mainFoo)
+			got := mainFoo.String()
 			want := "x.Foo"
 			if want != got {
 				t.Errorf("\nwant:\n\t%q\nbut got:\n\t%q", want, got)
 			}
 		})
 		t.Run("same package", func(t *testing.T) {
-			got := fmt.Sprintf("%s", Bar)
+			got := Bar.String()
 			want := "Bar"
 			if want != got {
 				t.Errorf("\nwant:\n\t%q\nbut got:\n\t%q", want, got)
--- pkg/tinypkg/write_something.go
+++ pkg/tinypkg/write_something.go
@@ -50,3 +50,126 @@ func WriteInterface(w io.Writer, here *Package, name string, iface *Interface) e
 	}
 	return nil
 }
+
+type Binding struct {
+	Name string
+
+	Provider      *Func
+	ProviderAlias string
+
+	HasError   bool
+	HasCleanup bool
+}
+
+var ErrUnexpectedReturnType = fmt.Errorf("unexpected-return-type")
+var ErrUnexpectedExternalReturnType = fmt.Errorf("unexpected-external-return-type")
+
+func NewBinding(name string, provider *Func) (*Binding, error) {
+	b := &Binding{Name: name, Provider: provider}
+	switch len(provider.Returns) {
+	case 1:
+		// noop
+	case 2:
+		if provider.Returns[1].Node.String() == "error" {
+			b.HasError = true
+		} else if _, ok := provider.Returns[1].Node.(*Func); ok {
+			// TODO: validation
+			b.HasCleanup = true
+		} else {
+			return nil, fmt.Errorf("invalid signature(2) %s, supported return type are (<T>, error) and (<T>, func(), %w", provider, ErrUnexpectedReturnType)
+		}
+	case 3:
+		if provider.Returns[2].Node.String() == "error" {
+			b.HasError = true
+		}
+		if _, ok := provider.Returns[1].Node.(*Func); ok {
+			// TODO: validation
+			b.HasCleanup = true
+		}
+		if !(b.HasError && b.HasCleanup) {
+			return nil, fmt.Errorf("invalid signature(3) %s, supported return type are (<T>, func(), error), %w", b.Provider, ErrUnexpectedReturnType)
+		}
+	default:
+		return nil, fmt.Errorf("invalid signature(N) %s, %w", provider, ErrUnexpectedReturnType)
+	}
+	return b, nil
+}
+
+// TODO: support non-pointer zero value
+// TODO: name-check (when calling provider function)
+
+func (b *Binding) WriteWithCallbackAndError(w io.Writer, here *Package, indent string, returns []*Var) error {
+	if 3 < len(returns) {
+		return fmt.Errorf("sorry the maximum value of supported number-of-return-value is 3, but %s is passed, %w", returns, ErrUnexpectedExternalReturnType)
+	}
+
+	fmt.Fprintf(w, "%svar %s %s\n", indent, b.Name, ToRelativeTypeString(here, b.Provider.Returns[0].Node))
+	fmt.Fprintf(w, "%s{\n", indent)
+	defer fmt.Fprintf(w, "%s}\n", indent)
+	{
+		if b.HasCleanup {
+			fmt.Fprintf(w, "%s\tvar cleanup func()\n", indent)
+		}
+		if b.HasError {
+			fmt.Fprintf(w, "%s\tvar err error\n", indent)
+		}
+
+		var callRHS string
+		{
+			provider := b.Provider
+			args := make([]string, 0, len(provider.Args))
+			for _, x := range provider.Args {
+				args = append(args, x.Name)
+			}
+			providerName := provider.Name
+			if b.ProviderAlias != "" {
+				providerName = b.ProviderAlias
+			}
+			callRHS = fmt.Sprintf("%s(%s)", providerName, strings.Join(args, ", "))
+		}
+
+		switch len(b.Provider.Returns) {
+		case 1:
+			fmt.Fprintf(w, "%s\t%s = %s\n", indent, b.Name, callRHS)
+		case 2:
+			if b.HasError {
+				fmt.Fprintf(w, "%s\t%s, err = %s\n", indent, b.Name, callRHS)
+			} else if b.HasCleanup {
+				fmt.Fprintf(w, "%s\t%s, cleanup = %s\n", indent, b.Name, callRHS)
+			} else {
+				return fmt.Errorf("invalid signature(2) %s, supported return type are (<T>, error) and (<T>, func(), %w", b.Provider, ErrUnexpectedReturnType)
+			}
+		case 3:
+			if b.HasError && b.HasCleanup {
+				fmt.Fprintf(w, "%s\t%s, cleanup, err = %s\n", indent, b.Name, callRHS)
+			} else {
+				return fmt.Errorf("invalid signature(3) %s, supported return type are (<T>, func(), error), %w", b.Provider, ErrUnexpectedReturnType)
+			}
+		default:
+			return fmt.Errorf("invalid signature(N) %s, %w", b.Provider, ErrUnexpectedReturnType)
+		}
+
+		if b.HasCleanup {
+			fmt.Fprintf(w, "%s\tif cleanup != nil {\n", indent)
+			fmt.Fprintf(w, "%s\t\tdefer cleanup()\n", indent)
+			fmt.Fprintf(w, "%s\t}\n", indent)
+		}
+		if b.HasError { // TODO: support zero-value
+			var returnRHS string
+			if len(returns) == 0 {
+				returnRHS = "panic(err) // TODO: fix-it"
+			} else {
+				values := []string{"nil", "nil", "nil"}
+				if returns[len(returns)-1].Node.String() == "error" {
+					values[len(returns)-1] = "err"
+				}
+				returnRHS = "return " + strings.Join(values[:len(returns)], ", ")
+			}
+
+			fmt.Fprintf(w, "%s\tif err != nil {\n", indent)
+			fmt.Fprintf(w, "%s\t\t%s\n", indent, returnRHS)
+			fmt.Fprintf(w, "%s\t}\n", indent)
+		}
+	}
+	return nil
+}
