72e498f9f2ea258865810f084b959e5b8169950c
podhmo <ababjam61+github@gmail.com>
2021-09-24 22:26:06 +0900

    feat: resolve def's return type

--- resolve/def.go
+++ resolve/def.go
@@ -1,6 +1,9 @@
 package resolve
 
 import (
+	"fmt"
+	"reflect"
+
 	"github.com/podhmo/apikit/pkg/tinypkg"
 	reflectshape "github.com/podhmo/reflect-shape"
 )
@@ -10,6 +13,9 @@ type Def struct {
 	Shape   reflectshape.Function
 	Args    []Item
 	Returns []Item
+
+	HasError   bool
+	HasCleanup bool
 }
 
 type Item struct {
@@ -48,6 +54,58 @@ func ExtractDef(universe *tinypkg.Universe, extractor *reflectshape.Extractor, f
 		Shape:   sfn,
 		Args:    args,
 		Returns: returns,
+	}
+}
+
+var ErrUnexpectedReturnType = fmt.Errorf("unexpected-return-type")
+var reflectErrorTypeValue = reflect.TypeOf(func() error { return nil }).Out(0)
+
+func bindReturnsInfo(def *Def) error {
+	returns := def.Returns
+	switch len(returns) {
+	case 0:
+		return nil
+	case 1:
+		lastValueType := returns[0].Shape.GetReflectType()
+		if lastValueType == reflectErrorTypeValue { // error
+			def.HasError = true
+			return nil
+		} else if lastValueType.Kind() == reflect.Func { // (<T>, func())
+			def.HasCleanup = true
+			// TODO: validation
+			return nil
+		}
+		return nil
+	case 2:
+		lastValueType := returns[1].Shape.GetReflectType()
+		if lastValueType == reflectErrorTypeValue { // (<T>, error)
+			def.HasError = true
+			return nil
+		} else if lastValueType.Kind() == reflect.Func { // (<T>, func())
+			def.HasCleanup = true
+			// TODO: validation
+			return nil
+		} else {
+			return fmt.Errorf("returns[2] signature invalid (%s): %w", def.Symbol, ErrUnexpectedReturnType)
+		}
+	case 3: // (<T>, func(), error)
+		lastValueType := returns[2].Shape.GetReflectType()
+		if lastValueType == reflectErrorTypeValue { // (<T>, error)
+			def.HasError = true
+		} else {
+			return fmt.Errorf("returns[3] signature invalid (%s): %w", def.Symbol, ErrUnexpectedReturnType)
+		}
+
+		sndValueTypeKind := returns[1].Shape.GetReflectKind()
+		if sndValueTypeKind == reflect.Func {
+			def.HasCleanup = true
+			// TODO: validation
+		} else {
+			return fmt.Errorf("returns[3] signature invalid (%s): %w", def.Symbol, ErrUnexpectedReturnType)
+		}
+		return nil
+	default:
+		return fmt.Errorf("returns[%d] signature invalid (%s): %w", len(returns), def.Symbol, ErrUnexpectedReturnType)
 	}
 }
 
