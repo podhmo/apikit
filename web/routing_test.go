83cc6fa6dd56bf41db0ca352faad12f9aeafaf4b
podhmo <ababjam61+github@gmail.com>
2021-09-24 17:59:23 +0900

    test: more tests

--- web/routing_test.go
+++ web/routing_test.go
@@ -11,30 +11,57 @@ import (
 )
 
 func TestRouting(t *testing.T) {
-	r := web.NewRouter()
-	r.Get("/articles/{articleId}", "get article")
-	r.Group("/articles/{articleId}", func(r *web.Router) {
-		r.Get("/comments", "list comments")
-		r.Get("/comments/{commentId}/", "get comment")
-	})
-
-	var paths []string
-	if err := web.Walk(r, func(n *web.WalkerNode) error {
-		paths = append(paths, strings.Join(n.Path(), ""))
-		return nil
-	}); err != nil {
-		t.Fatalf("unexpected error %+v", err)
+	cases := []struct {
+		msg    string
+		router *web.Router
+		want   []string
+	}{
+		{
+			msg: "one",
+			router: func() *web.Router {
+				r := web.NewRouter()
+				r.Get("/articles/{articleId}", "get article")
+				return r
+			}(),
+			want: []string{
+				"GET /articles/{articleId}",
+			},
+		},
+		{
+			msg: "many",
+			router: func() *web.Router {
+				r := web.NewRouter()
+				r.Get("/articles/{articleId}", "get article")
+				r.Group("/articles/{articleId}", func(r *web.Router) {
+					r.Get("/comments", "list comments")
+					r.Get("/comments/{commentId}/", "get comment")
+				})
+				return r
+			}(),
+			want: []string{
+				"GET /articles/{articleId}",
+				"GET /articles/{articleId}/comments",
+				"GET /articles/{articleId}/comments/{commentId}",
+			},
+		},
 	}
 
-	want := []string{
-		"GET /articles/{articleId}",
-		"GET /articles/{articleId}/comments",
-		"GET /articles/{articleId}/comments/{commentId}",
-	}
+	for _, c := range cases {
+		c := c
+		t.Run(c.msg, func(t *testing.T) {
+			var paths []string
+			if err := web.Walk(c.router, func(n *web.WalkerNode) error {
+				paths = append(paths, strings.Join(n.Path(), ""))
+				return nil
+			}); err != nil {
+				t.Fatalf("unexpected error %+v", err)
+			}
 
-	if got := paths; !reflect.DeepEqual(want, got) {
-		sort.Strings(want)
-		sort.Strings(got)
-		difftest.LogDiffGotStringAndWantString(t, strings.Join(got, "\n"), strings.Join(want, "\n"))
+			if want, got := c.want, paths; !reflect.DeepEqual(want, got) {
+				sort.Strings(want)
+				sort.Strings(got)
+				difftest.LogDiffGotStringAndWantString(t, strings.Join(got, "\n"), strings.Join(want, "\n"))
+			}
+		})
 	}
 }
