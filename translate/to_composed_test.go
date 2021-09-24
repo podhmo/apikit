12c33a9285b04b5a3adf040d611cf1b86575aaf7
podhmo <ababjam61+github@gmail.com>
2021-09-25 03:28:21 +0900

    test: to table driven test

--- translate/to_composed_test.go
+++ translate/to_composed_test.go
@@ -1,9 +1,11 @@
 package translate
 
 import (
-	"os"
+	"reflect"
+	"strings"
 	"testing"
 
+	"github.com/podhmo/apikit/pkg/difftest"
 	"github.com/podhmo/apikit/pkg/tinypkg"
 	"github.com/podhmo/apikit/resolve"
 )
@@ -24,16 +26,82 @@ func NewEvent(g Greeter) (*Event, error) {
 
 func TestToComposed(t *testing.T) {
 	resolver := resolve.NewResolver()
-	here := tinypkg.NewPackage("main", "")
+	main := resolver.NewPackage("main", "")
+	pkg := resolver.NewPackage(reflect.TypeOf(DB{}).PkgPath(), "")
 
-	inputs := []*resolve.Def{
-		resolver.Def(NewMessage),
-		resolver.Def(NewGreeter),
-		resolver.Def(NewEvent),
+	cases := []struct {
+		msg   string
+		here  *tinypkg.Package
+		input []interface{}
+		want  string
+	}{
+		{
+			msg:  "another-package",
+			here: main,
+			input: []interface{}{
+				NewMessage,
+				NewGreeter,
+				NewEvent,
+			},
+			want: `
+func InitializeEvent(phrase string) (*translate.Event, error) {
+	var v0 *translate.Message
+	{
+		v0 = NewMessage(phrase)
+	}
+	var v1 *translate.Greeter
+	{
+		var err error
+		v1, err = NewGreeter(m)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return translate.NewEvent(v1)
+}`,
+		},
+		{
+			msg:  "same-package",
+			here: pkg,
+			input: []interface{}{
+				NewMessage,
+				NewGreeter,
+				NewEvent,
+			},
+			want: `
+func InitializeEvent(phrase string) (*Event, error) {
+	var v0 *Message
+	{
+		v0 = NewMessage(phrase)
+	}
+	var v1 *Greeter
+	{
+		var err error
+		v1, err = NewGreeter(m)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return NewEvent(v1)
+}`,
+		},
 	}
 
-	w := os.Stdout
-	if err := writeComposed(w, here, resolver, "InitializeEvent", inputs); err != nil {
-		t.Errorf("unexpected error %+v", err)
+	for _, c := range cases {
+		c := c
+		t.Run(c.msg, func(t *testing.T) {
+			providers := make([]*resolve.Def, len(c.input))
+			for i, fn := range c.input {
+				providers[i] = resolver.Def(fn)
+			}
+
+			var buf strings.Builder
+			if err := writeComposed(&buf, c.here, resolver, "InitializeEvent", providers); err != nil {
+				t.Errorf("unexpected error %+v", err)
+			}
+			if want, got := strings.TrimSpace(c.want), strings.TrimSpace(buf.String()); want != got {
+				difftest.LogDiffGotStringAndWantString(t, got, want)
+			}
+		})
 	}
 }
