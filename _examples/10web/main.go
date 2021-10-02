734b5cb242e188c53f13b1d53c9180c28bdef9ff
podhmo <ababjam61+github@gmail.com>
2021-10-02 19:17:05 +0900

    chore: update examples (but not complete)

--- _examples/10web/main.go
+++ _examples/10web/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 	"strings"
 
-	"m/10routing/design"
+	"m/10web/design"
 
+	"github.com/podhmo/apikit/pkg/tinypkg"
 	"github.com/podhmo/apikit/web"
+	"github.com/podhmo/apikit/web/webtranslate"
 )
 
 func main() {
@@ -23,6 +26,10 @@ func main() {
 // TODO: set error handler (500-handler)
 // TODO: set 404-handler
 
+// TODO: normalize argname and path parameter. (e.g. articleID and articleId)
+// TODO: gentle message ( extract path info: expected variables are [], but want variables are [articleId] (in def GetArticle): mismatch-number-of-variables)
+// - which is wrong? (routing?, func-def?)
+
 func run() (err error) {
 	r := web.NewRouter()
 	r.Group("/articles", func(r *web.Router) {
@@ -33,11 +40,49 @@ func run() (err error) {
 	})
 
 	enc := json.NewEncoder(os.Stdout)
+
+	fmt.Println("----------------------------------------")
 	web.Walk(r, func(n *web.WalkerNode) error {
 		return enc.Encode(map[string]interface{}{
 			"path": strings.Join(n.Path(), ""),
 			"vars": n.Node.VariableNames,
 		})
 	})
-	return nil
+	fmt.Println("----------------------------------------")
+
+	translator := webtranslate.NewTranslator(webtranslate.DefaultConfig())
+
+	{
+		resolver := translator.Config.Resolver
+		tracker := translator.Tracker
+		runtime := resolver.NewPackage("m/runtime", "")
+		main := resolver.NewPackage("m/main", "")
+
+		w := os.Stdout
+		here := main
+
+		return web.Walk(r, func(node *web.WalkerNode) error {
+			def := resolver.Def(node.Node.Value)
+			tracker.Track(def)
+			pathinfo, err := web.ExtractPathInfo(node.Node.VariableNames, def)
+			if err != nil {
+				return fmt.Errorf("extract path info: %w", err)
+			}
+
+			providerFunc := here.NewFunc( // todo: simplify
+				"getProvider",
+				[]*tinypkg.Var{{Node: &tinypkg.Pointer{Lv: 1, V: tinypkg.NewPackage("net/http", "").NewSymbol("Request")}}},
+				[]*tinypkg.Var{
+					{Node: &tinypkg.Pointer{Lv: 1, V: tinypkg.NewPackage("net/http", "").NewSymbol("Request")}},
+					{Node: here.NewSymbol("Provider")},
+					{Node: tinypkg.NewSymbol("error")},
+				},
+			)
+
+			if err := webtranslate.WriteHandlerFunc(w, here, resolver, tracker, pathinfo, runtime, providerFunc, ""); err != nil {
+				return fmt.Errorf("write: %w", err)
+			}
+			return nil
+		})
+	}
 }
