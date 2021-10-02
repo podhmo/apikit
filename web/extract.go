210dade52784e7c5204e249af8f89a608a3ab98a
podhmo <ababjam61+github@gmail.com>
2021-10-02 21:48:54 +0900

    fix: normalized pathvar name

--- web/extract.go
+++ web/extract.go
@@ -4,20 +4,21 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/podhmo/apikit/namelib"
 	"github.com/podhmo/apikit/resolve"
 	reflectshape "github.com/podhmo/reflect-shape"
 )
 
 type PathVar struct {
-	Name  string
-	Shape reflectshape.Shape
+	Name  string // not argname (e.g. /{articleId} and func(articleID string) { ... })
 	Regex string
+	Shape reflectshape.Shape
 }
 
 type PathInfo struct {
 	Def      *resolve.Def
 	VarNames []string
-	Vars     map[string]*PathVar
+	Vars     map[string]*PathVar // argname -> PathVar
 }
 
 var (
@@ -25,46 +26,62 @@ var (
 	ErrMismatchNumberOfVariables = fmt.Errorf("mismatch-number-of-variables")
 )
 
-func ExtractPathInfo(variableNames []string, def *resolve.Def) (*PathInfo, error) {
-	vars := map[string]*PathVar{}
-	idx := 0
+func ExtractPathInfo(pathParameters []string, def *resolve.Def) (*PathInfo, error) {
+	argNames := make([]string, 0, len(pathParameters))
+	vars := make(map[namelib.Normalized]*PathVar, len(pathParameters))
 	for _, item := range def.Args {
 		if item.Kind != resolve.KindPrimitive {
 			continue
 		}
 
-		var regex string
 		argname := item.Name
-		if strings.Contains(argname, ":") {
-			nameAndRegex := strings.SplitN(argname, ":", 2)
-			argname = nameAndRegex[0]
-			regex = nameAndRegex[1]
-		}
+		normalized := namelib.ToNormalized(argname)
 
-		if len(variableNames) <= idx {
-			got := make([]string, 0, len(vars))
-			for _, v := range vars {
-				got = append(got, v.Name)
-			}
-			return nil, fmt.Errorf("extracted path-parameters are %v, but extracted arguments from function are %v (in def %s): %w", got, variableNames, def, ErrMismatchNumberOfVariables)
+		argNames = append(argNames, argname)
+		vars[normalized] = &PathVar{
+			Name:  argname,
+			Shape: item.Shape,
 		}
-		if argname != variableNames[idx] {
-			continue
-		}
-		vars[argname] = &PathVar{Name: argname, Regex: regex, Shape: item.Shape}
-		idx++
 	}
 
-	if len(vars) != len(variableNames) {
-		got := make([]string, 0, len(vars))
-		for _, v := range vars {
-			got = append(got, v.Name)
+	if len(argNames) != len(pathParameters) {
+		want := argNames
+		got := pathParameters
+		return nil, fmt.Errorf("path-parameters are %v, but extracted values are %v (in %s): %w", got, want, def, ErrMismatchNumberOfVariables)
+	}
+
+	missingParams := make([]string, 0, len(pathParameters))
+	for _, name := range pathParameters {
+		var regex string
+		if strings.Contains(name, ":") {
+			nameAndRegex := strings.SplitN(name, ":", 2)
+			name = nameAndRegex[0]
+			regex = nameAndRegex[1]
 		}
-		return nil, fmt.Errorf("extracted path-parameters are %v, but extracted arguments from function are %v (in def %s): %w", got, variableNames, def, ErrMismatchNumberOfVariables)
+		normalized := namelib.ToNormalized(name)
+		item, ok := vars[normalized]
+		if !ok {
+			missingParams = append(missingParams, name)
+			continue
+		}
+
+		item.Name = name
+		item.Regex = regex
+	}
+
+	if len(missingParams) > 0 {
+		want := argNames
+		got := missingParams
+		return nil, fmt.Errorf("parameters %v are missing (arguments=%v, in %s): %w", got, want, def, ErrMismatchNumberOfVariables)
+	}
+
+	argVars := make(map[string]*PathVar)
+	for _, name := range argNames {
+		argVars[name] = vars[namelib.ToNormalized(name)]
 	}
 	return &PathInfo{
 		Def:      def,
-		VarNames: variableNames,
-		Vars:     vars,
+		VarNames: argNames,
+		Vars:     argVars,
 	}, nil
 }
