84d3e41bdf23115ae878f6915eb40164f01fa572
podhmo <ababjam61+github@gmail.com>
2021-10-05 20:38:09 +0900

    test: add tests (broken)

--- resolve/pre_module_test.go
+++ resolve/pre_module_test.go
@@ -44,36 +44,63 @@ func TestNewPreModule(t *testing.T) {
 }
 
 func TestNewModule(t *testing.T) {
-	here := tinypkg.NewPackage("main", "")
-
 	resolver := NewResolver()
 	target := GetProvider{}
+
 	pm, err := resolver.PreModule(target)
 	if err != nil {
 		t.Fatalf("new pre module %+v", err)
 	}
 
-	m, err := pm.NewModule(here, here.NewSymbol("Provider"))
-	if err != nil {
-		t.Fatalf("new module %+v", err)
+	main := tinypkg.NewPackage("main", "")
+	cases := []struct {
+		msg  string
+		here *tinypkg.Package
+		args []tinypkg.Node
+		want map[string]string
+	}{
+		{
+			msg:  "same package",
+			here: main,
+			args: []tinypkg.Node{main.NewSymbol("Provider")},
+			want: map[string]string{
+				"GetProvider": `func(*resolve.Request) (*resolve.Request, Provider, error)`,
+			},
+		},
+		{
+			msg:  "another package",
+			here: main,
+			args: []tinypkg.Node{tinypkg.NewPackage("provider", "").NewSymbol("Provider")},
+			want: map[string]string{
+				"GetProvider": `func(*resolve.Request) (*resolve.Request, provider.Provider, error)`,
+			},
+		},
 	}
+	for _, c := range cases {
+		t.Run(c.msg, func(t *testing.T) {
+			m, err := pm.NewModule(c.here, c.args...)
+			if err != nil {
+				t.Fatalf("new module %+v", err)
+			}
+			t.Run("string", func(t *testing.T) {
+				want := `Module[here=main, args=[Provider], funcs=[GetProvider unexported]]`
+				got := m.String()
+				if want != got {
+					t.Errorf("want String()\n\t%s\nbut got\n\t%s", want, got)
+				}
+			})
 
-	t.Run("string", func(t *testing.T) {
-		want := `Module[here=main, args=[Provider], funcs=[GetProvider unexported]]`
-		got := m.String()
-		if want != got {
-			t.Errorf("want String()\n\t%s\nbut got\n\t%s", want, got)
-		}
-	})
-	t.Run("funcs", func(t *testing.T) {
-		name := "GetProvider"
-		want := `func(*resolve.Request) (*resolve.Request, Provider, error)`
-		f, err := m.Type(name)
-		if err != nil {
-			t.Errorf("Func() %+v", err)
-		}
-		if got := f.String(); want != got {
-			t.Errorf("want String()\n\t%s\nbut got\n\t%s", want, got)
-		}
-	})
+			t.Run("func", func(t *testing.T) {
+				for name, want := range c.want {
+					f, err := m.Type(name)
+					if err != nil {
+						t.Errorf("Func() %+v", err)
+					}
+					if got := f.String(); want != got {
+						t.Errorf("want String()\n\t%s\nbut got\n\t%s", want, got)
+					}
+				}
+			})
+		})
+	}
 }
