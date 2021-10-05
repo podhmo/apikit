fc65edfa3dabefe709d043ea6f1007343525cc9f
podhmo <ababjam61+github@gmail.com>
2021-10-05 23:55:36 +0900

    test: fix tests

--- translate/to_interface_test.go
+++ translate/to_interface_test.go
@@ -27,6 +27,7 @@ func TestInterface(t *testing.T) {
 	pkg := resolver.NewPackage(reflect.TypeOf(DB{}).PkgPath(), "")
 
 	config := DefaultConfig()
+	config.Header = ""
 	config.Resolver = resolver
 
 	cases := []struct {
@@ -42,10 +43,12 @@ func TestInterface(t *testing.T) {
 			msg:   "1 component, another package",
 			here:  main,
 			input: []interface{}{ListUser},
-			want: `
+			want: `package main
+
 import (
 	"github.com/podhmo/apikit/translate"
 )
+
 type Component interface {
 	DB() *translate.DB
 }`,
@@ -54,7 +57,9 @@ type Component interface {
 			msg:   "1 component, same package",
 			here:  pkg,
 			input: []interface{}{ListUser},
-			want: `
+			want: `package translate
+
+
 type Component interface {
 	DB() *DB
 }`,
@@ -64,10 +69,12 @@ type Component interface {
 			msg:   "1 component, N actions, another package",
 			here:  main,
 			input: []interface{}{ListUser, ListName, ListUserWithError},
-			want: `
+			want: `package main
+
 import (
 	"github.com/podhmo/apikit/translate"
 )
+
 type Component interface {
 	DB() *translate.DB
 }`,
@@ -76,10 +83,12 @@ type Component interface {
 			msg:   "same types but with another name, another package",
 			here:  main,
 			input: []interface{}{ListUser, ListUserFromAnotherDB},
-			want: `
+			want: `package main
+
 import (
 	"github.com/podhmo/apikit/translate"
 )
+
 type Component interface {
 	Db() *translate.DB
 	AnotherDb() *translate.DB
@@ -94,10 +103,12 @@ type Component interface {
 				def := resolver.Def(func() (*DB, error) { return nil, nil })
 				tracker.Override(rt, "db", def)
 			},
-			want: `
+			want: `package main
+
 import (
 	"github.com/podhmo/apikit/translate"
 )
+
 type Component interface {
 	DB() (*translate.DB, error)
 }`,
@@ -111,11 +122,13 @@ type Component interface {
 				def := resolver.Def(func(ctx context.Context) (*DB, error) { return nil, nil })
 				tracker.Override(rt, "db", def)
 			},
-			want: `
+			want: `package main
+
 import (
 	"context"
 	"github.com/podhmo/apikit/translate"
 )
+
 type Component interface {
 	DB(ctx context.Context) (*translate.DB, error)
 }`,
@@ -135,17 +148,7 @@ type Component interface {
 
 			code := translator.TranslateToInterface(c.here, "Component")
 			var buf strings.Builder
-			collector := tinypkg.NewImportCollector(c.here)
-			if err := code.CollectImports(collector); err != nil && c.wantError == nil || c.wantError != err {
-				t.Fatalf("unexpected error, collect imports %+v", err)
-			}
-			imports := collector.Imports
-			if err := code.EmitImports(&buf, imports); err != nil {
-				if c.wantError == nil || c.wantError != err {
-					t.Fatalf("unexpected error, import %+v", err)
-				}
-			}
-			if err := code.EmitCode(&buf); err != nil {
+			if err := config.EmitCode(&buf, code); err != nil {
 				if c.wantError == nil || c.wantError != err {
 					t.Fatalf("unexpected error, code %+v", err)
 				}
