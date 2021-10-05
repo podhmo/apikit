557e2934102e7d6a43c2c5019bdb0bb961b12f01
podhmo <ababjam61+github@gmail.com>
2021-10-05 19:06:00 +0900

    chore: add examples (broken)

--- _examples/10web/main.go
+++ _examples/10web/main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
+	"m/10web/design"
 	"os"
 	"strings"
 
-	"m/10web/design"
-
 	"github.com/podhmo/apikit/web"
-	"github.com/podhmo/apikit/web/webtranslate"
 )
 
 func main() {
@@ -18,16 +15,6 @@ func main() {
 		log.Fatalf("!! %+v", err)
 	}
 }
-
-// TODO: code generation
-// TODO: parameters bindings
-// TODO: set lift function
-// TODO: set error handler (500-handler)
-// TODO: set 404-handler
-
-// TODO: normalize argname and path parameter. (e.g. articleID and articleId)
-// TODO: gentle message ( extract path info: expected variables are [], but want variables are [articleId] (in def GetArticle): mismatch-number-of-variables)
-// - which is wrong? (routing?, func-def?)
 
 func run() (err error) {
 	r := web.NewRouter()
@@ -39,48 +26,10 @@ func run() (err error) {
 	})
 
 	enc := json.NewEncoder(os.Stdout)
-
-	fmt.Println("----------------------------------------")
-	web.Walk(r, func(n *web.WalkerNode) error {
+	return web.Walk(r, func(n *web.WalkerNode) error {
 		return enc.Encode(map[string]interface{}{
 			"path": strings.Join(n.Path(), ""),
 			"vars": n.Node.VariableNames,
 		})
 	})
-	fmt.Println("----------------------------------------")
-
-	translator := webtranslate.NewTranslator(webtranslate.DefaultConfig())
-
-	{
-		resolver := translator.Config.Resolver
-		tracker := translator.Tracker
-		runtime := resolver.NewPackage("m/runtime", "")
-		main := resolver.NewPackage("m/main", "")
-
-		w := os.Stdout
-		here := main
-
-		translator.Override("db", design.NewDB)
-
-		getProviderModule, err := translator.GetProviderModule(here, "Provider")
-		if err != nil {
-			return err
-		}
-		runtimeModule, err := translator.RuntimeModule(runtime)
-		if err != nil {
-			return err
-		}
-		return web.Walk(r, func(node *web.WalkerNode) error {
-			def := resolver.Def(node.Node.Value)
-			tracker.Track(def)
-			pathinfo, err := web.ExtractPathInfo(node.Node.VariableNames, def)
-			if err != nil {
-				return fmt.Errorf("extract path info: %w", err)
-			}
-			if err := webtranslate.WriteHandlerFunc(w, here, resolver, tracker, pathinfo, getProviderModule, runtimeModule, ""); err != nil {
-				return fmt.Errorf("write: %w", err)
-			}
-			return nil
-		})
-	}
 }
---
+++ _examples/11web/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"m/11web/design"
+
+	"github.com/podhmo/apikit/pkg/emitgo"
+	"github.com/podhmo/apikit/web"
+	"github.com/podhmo/apikit/web/webtranslate"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatalf("!! %+v", err)
+	}
+}
+
+// TODO: code generation
+// TODO: parameters bindings
+// TODO: set lift function
+// TODO: set error handler (500-handler)
+// TODO: set 404-handler
+
+// TODO: normalize argname and path parameter. (e.g. articleID and articleId)
+// TODO: gentle message ( extract path info: expected variables are [], but want variables are [articleId] (in def GetArticle): mismatch-number-of-variables)
+// - which is wrong? (routing?, func-def?)
+
+// TODO: generate routing
+
+func run() (err error) {
+	emitter := emitgo.NewFromRelativePath(design.ListArticle, "..")
+	defer emitter.EmitWith(&err)
+	rootpkg := emitter.RootPkg
+
+	r := web.NewRouter()
+	r.Group("/articles", func(r *web.Router) {
+		// TODO: set tag
+
+		r.Get("/", design.ListArticle)
+		r.Get("/{articleId}", design.GetArticle)
+	})
+
+	enc := json.NewEncoder(os.Stdout)
+
+	fmt.Println("----------------------------------------")
+	web.Walk(r, func(n *web.WalkerNode) error {
+		return enc.Encode(map[string]interface{}{
+			"path": strings.Join(n.Path(), ""),
+			"vars": n.Node.VariableNames,
+		})
+	})
+	fmt.Println("----------------------------------------")
+
+	translator := webtranslate.NewTranslator(webtranslate.DefaultConfig())
+	resolver := translator.Resolver
+
+	translator.Config.RuntimePkg = resolver.NewPackage("github.com/podhmo/apikit/web/webruntime", "") // xxx
+	pkg := rootpkg.Relative("handler", "")
+
+	translator.Override("db", design.NewDB)
+
+	{
+		here := pkg
+		if err := web.Walk(r, func(node *web.WalkerNode) error {
+			code := translator.TranslateToHandler(here, node, "")
+			emitter.Register(here, code.Name, code)
+			return nil
+		}); err != nil {
+			return err
+		}
+	}
+
+	{
+		here := pkg
+		code := translator.TranslateToInterface(here)
+		emitter.Register(here, code.Name, code)
+	}
+	return nil
+}
