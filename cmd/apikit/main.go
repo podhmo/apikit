9b30b5bb78b410126d9353121567b588c6dfb659
podhmo <ababjam61+github@gmail.com>
2021-10-10 02:34:53 +0900

    feat: init command with separated output

--- cmd/apikit/main.go
+++ cmd/apikit/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/podhmo/apikit/cmd/apikit/internal/clilib"
+	"github.com/podhmo/apikit/cmd/apikit/internal/initcmd"
 )
 
 func main() {
@@ -14,7 +13,7 @@ func main() {
 
 	name := os.Args[0]
 	subCommands := []*clilib.Command{
-		NewInitCommand(),
+		initcmd.New(),
 	}
 
 	cmd := clilib.NewRouterCommand(name, subCommands)
@@ -23,39 +22,5 @@ func main() {
 	}
 	if err := run(); err != nil {
 		log.Fatalf("!! %+v", err)
-	}
-}
-
-func NewInitCommand() *clilib.Command {
-	fs := flag.NewFlagSet("init", flag.ExitOnError)
-	var options struct {
-		Verbose bool `json:"verbose"`
-	}
-	fs.BoolVar(&options.Verbose, "verbose", false, "verbose option")
-
-	fs.Usage = func() {
-		name := fs.Name()
-		fmt.Fprintf(fs.Output(), "Usage of %s <path>:\n\n", name)
-		fs.PrintDefaults()
-		fmt.Fprintln(fs.Output(), "  path")
-		fmt.Fprintln(fs.Output(), "\tthe path of directory")
-	}
-
-	return &clilib.Command{
-		FlagSet: fs,
-		Options: &options,
-		Do: func(path []*clilib.Command, args []string) error {
-			cmd := path[len(path)-1]
-			if err := cmd.Parse(args); err != nil {
-				return err
-			}
-			if cmd.NArg() < 1 {
-				cmd.Usage()
-				return fmt.Errorf("init <path>")
-			}
-
-			fmt.Println("@@", "hello", options.Verbose)
-			return nil
-		},
 	}
 }
