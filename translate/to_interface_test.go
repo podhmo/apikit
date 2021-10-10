1c9ca30a9add03f0c97c40e0070180509813a71c
podhmo <ababjam61+github@gmail.com>
2021-10-10 17:51:02 +0900

    tests: add tests

--- translate/to_interface_test.go
+++ translate/to_interface_test.go
@@ -1,10 +1,13 @@
 package translate
 
 import (
-	"fmt"
 	"io"
+	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/podhmo/apikit/pkg/difftest"
+	"github.com/podhmo/apikit/pkg/tinypkg"
 )
 
 type something struct{}
@@ -14,19 +17,63 @@ func (s *something) Name() string                       { return "hello" }
 func (s *something) Emit(w io.Writer) (CodeType, error) { return CodeType("misc"), nil }
 
 func TestStructToInterface(t *testing.T) {
-	ob := &something{}
-
 	config := DefaultConfig()
 	config.Header = ""
 	translator := NewTranslator(config)
 	resolver := translator.Resolver
 
 	main := resolver.NewPackage("main", "")
-	code := translator.TranslateToInterface(main, ob, "EmitTarget")
+	pkg := resolver.NewPackage(reflect.TypeOf(something{}).PkgPath(), "")
 
-	var buf strings.Builder
-	if err := code.Emit(&buf); err != nil {
-		t.Fatalf("unexpected error %+v", err)
+	cases := []struct {
+		msg   string
+		here  *tinypkg.Package
+		input interface{}
+		want  string
+	}{
+		{
+			msg:   "same-package",
+			here:  pkg,
+			input: &something{},
+			want:  `package translate
+
+import (
+	"io"
+)
+
+type Generated interface {
+	Emit(io.Writer) (CodeType, error)
+	Name() string
+}`,
+		},
+		{
+			msg:   "other-package",
+			here:  main,
+			input: &something{},
+			want:  `package main
+
+import (
+	"io"
+	"github.com/podhmo/apikit/translate"
+)
+
+type Generated interface {
+	Emit(io.Writer) (translate.CodeType, error)
+	Name() string
+}`,
+		},
 	}
-	fmt.Println(buf.String())
+	for _, c := range cases {
+		c := c
+		t.Run(c.msg, func(t *testing.T) {
+			code := translator.TranslateToInterface(c.here, c.input, "Generated")
+			var buf strings.Builder
+			if err := code.Emit(&buf); err != nil {
+				t.Fatalf("unexpected error %+v", err)
+			}
+			if want, got := strings.TrimSpace(c.want), strings.TrimSpace(buf.String()); want != got {
+				difftest.LogDiffGotStringAndWantString(t, got, want)
+			}
+		})
+	}
 }
