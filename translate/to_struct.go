8deb6fbdf3dd2f06ac1b63178a33d1da4b289161
podhmo <ababjam61+github@gmail.com>
2021-11-03 11:02:02 +0900

    feat: use option in TranslateToStruct() (tentative)

--- translate/to_struct.go
+++ translate/to_struct.go
@@ -13,22 +13,47 @@ import (
 	reflectshape "github.com/podhmo/reflect-shape"
 )
 
+type ToStructConfig struct {
+	name    string
+	tagsMap map[string]string
+}
+type ToStructOption func(*ToStructConfig)
+
+type ToStructNamespace struct{}
+
+func (t *Translator) OptionToStruct() *ToStructNamespace {
+	return nil // for namespace, so, actual value is not needed.
+}
+
+func (ns *ToStructNamespace) WithName(name string) ToStructOption {
+	return func(c *ToStructConfig) { c.name = name }
+}
+func (ns *ToStructNamespace) WithTag(pairs ...string) ToStructOption {
+	return func(c *ToStructConfig) {
+		for i := 0; i+1 < len(pairs); i += 2 {
+			c.tagsMap[pairs[i]] = pairs[i+1]
+		}
+	}
+}
+
 // TranslateToStruct translates to struct from function or concrete struct
 func (t *Translator) TranslateToStruct(
 	here *tinypkg.Package,
 	ob interface{},
-	name string,
-	tagsMap map[string]string,
+	options ...ToStructOption,
 ) *code.CodeEmitter {
 	shape := t.Resolver.Shape(ob)
+	cfg := &ToStructConfig{tagsMap: map[string]string{}}
+	for _, opt := range options {
+		opt(cfg)
+	}
+	if cfg.name == "" {
+		cfg.name = shape.GetName()
+	}
 
-	if name == "" {
-		name = shape.GetName()
-	}
-	if tagsMap == nil {
-		tagsMap = map[string]string{}
-	}
+	name := cfg.name
 	var s *Struct // bound by EmitCode()
+
 	c := &code.Code{
 		Name:   name,
 		Here:   here,
@@ -41,7 +66,7 @@ func (t *Translator) TranslateToStruct(
 			here := c.Here
 			switch shape := shape.(type) {
 			case reflectshape.Struct:
-				s = toStruct(here, resolver, name, shape, tagsMap)
+				s = toStruct(here, resolver, shape, *cfg)
 				if err := writeStruct(w, here, name, s, resolver); err != nil {
 					return fmt.Errorf("write struct: %w", err)
 				}
@@ -51,9 +76,9 @@ func (t *Translator) TranslateToStruct(
 				if err != nil {
 					return fmt.Errorf("transform function to struct: %w", err)
 				}
-				s = toStruct(here, resolver, name, structShape, tagsMap)
+				s = toStruct(here, resolver, structShape, *cfg)
 				if err := writeStruct(w, here, name, s, resolver); err != nil {
-					return err
+					return fmt.Errorf("write struct: %w", err)
 				}
 				return nil
 			default:
@@ -117,10 +142,12 @@ type StructField struct {
 func toStruct(
 	here *tinypkg.Package,
 	resolver *resolve.Resolver,
-	name string,
 	s reflectshape.Struct,
-	tagsMap map[string]string,
+	cfg ToStructConfig,
 ) *Struct {
+	name := cfg.name
+	tagsMap := cfg.tagsMap
+
 	fields := make([]StructField, s.Fields.Len())
 	for i, fieldname := range s.Fields.Keys {
 		f := s.Fields.Values[i]
