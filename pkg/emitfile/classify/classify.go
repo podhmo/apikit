78ff103599a008405a708b51f230c41c1c93c712
podhmo <ababjam61+github@gmail.com>
2021-11-13 18:50:16 +0900

    feat: emitfile/classify package

---
+++ pkg/emitfile/classify/classify.go
@@ -0,0 +1,109 @@
+package classify
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type Entry interface {
+	Name() string
+	Hash() ([]byte, error)
+}
+
+type funcEntry struct {
+	name     string
+	hashFunc func() ([]byte, error)
+}
+
+func NewEntry(name string, hashFunc func() ([]byte, error)) Entry {
+	return &funcEntry{
+		name:     name,
+		hashFunc: hashFunc,
+	}
+}
+func (e *funcEntry) Name() string {
+	return e.name
+}
+func (e *funcEntry) Hash() ([]byte, error) {
+	return e.hashFunc()
+}
+
+// TODO: concurrent
+
+func Classify(
+	prevEntries, entries []Entry,
+) ([]Result, error) {
+	Results := make([]Result, len(entries))
+
+	prevUsedCounter := make([]int, len(prevEntries))
+	hasPrev := len(prevEntries) > 0
+
+	for i, entry := range entries {
+		if !hasPrev {
+			Results[i] = Result{Type: ResultTypeCreate, Entry: entry}
+			continue
+		}
+
+		var prev Entry
+		name := entry.Name()
+		for j, x := range prevEntries {
+			if x.Name() == name {
+				prevUsedCounter[j]++
+				prev = x
+				break
+			}
+		}
+
+		if prev == nil {
+			Results[i] = Result{Type: ResultTypeCreate, Entry: entry}
+			continue
+		}
+
+		currentHash, err := entry.Hash()
+		if err != nil {
+			return nil, fmt.Errorf("get hash of current entry=%s: %w", name, err)
+		}
+		prevHash, err := prev.Hash()
+		if err != nil {
+			return nil, fmt.Errorf("get hash of previous entry=%s: %w", name, err)
+		}
+		if bytes.Equal(currentHash, prevHash) {
+			Results[i] = Result{Type: ResultTypeNotModified, Entry: entry}
+			continue
+		}
+		Results[i] = Result{Type: ResultTypeUpdate, Entry: entry}
+	}
+
+	if hasPrev {
+		for i, c := range prevUsedCounter {
+			if c == 0 {
+				entry := prevEntries[i]
+				Results = append(Results, Result{Type: ResultTypeDelete, Entry: entry})
+			}
+		}
+	}
+	return Results, nil
+}
+
+type ResultType string
+
+const (
+	ResultTypeUNKNOWN     ResultType = ""
+	ResultTypeCreate      ResultType = "C"
+	ResultTypeUpdate      ResultType = "U"
+	ResultTypeDelete      ResultType = "D"
+	ResultTypeNotModified ResultType = "-"
+)
+
+type Result struct {
+	Type  ResultType
+	Entry Entry
+}
+
+func (a Result) Name() string {
+	return a.Entry.Name()
+}
+
+func (a Result) String() string {
+	return fmt.Sprintf("%s %s", a.Type, a.Entry.Name())
+}
