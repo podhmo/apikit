1fd74b798d5f062bfab2f27b627b9461348bcc85
podhmo <ababjam61+github@gmail.com>
2021-11-27 13:34:05 +0900

    fix: emitgo.PackagePath, support more types fix #173

--- pkg/emitgo/inspect.go
+++ pkg/emitgo/inspect.go
@@ -9,12 +9,24 @@ import (
 
 func PackagePath(ob interface{}) string {
 	rt := reflect.TypeOf(ob)
-	if pkgPath := rt.PkgPath(); pkgPath != "" {
-		return pkgPath
+	if pkg := pkgPath(rt); pkg != "" {
+		return pkg
 	}
 	rfunc := runtime.FuncForPC(reflect.ValueOf(ob).Pointer())
 	parts := strings.Split(rfunc.Name(), ".") // method is not supported
 	return strings.Join(parts[:len(parts)-1], ".")
+}
+
+func pkgPath(rt reflect.Type) string {
+	pkg := rt.PkgPath()
+	if pkg != "" {
+		return pkg
+	}
+	switch rt.Kind() {
+	case reflect.Slice, reflect.Array, reflect.Ptr, reflect.Map:
+		return pkgPath(rt.Elem())
+	}
+	return pkg
 }
 
 func DefinedDir(fn interface{}) string {
