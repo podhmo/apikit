012eaa7af5c1d2721914df59c33242465928f1ad
podhmo <ababjam61+github@gmail.com>
2021-11-30 04:40:07 +0900

    test: add tests with adding CodeEmitter as dependency

--- code/merge_code_test.go
+++ code/merge_code_test.go
@@ -91,3 +91,36 @@ func Bar() {
 		}
 	})
 }
+
+func TestMergeCodeWithCodeEmitter(t *testing.T) {
+	config := DefaultConfig()
+	config.Header = ""
+	pkg := tinypkg.NewPackage("foo", "")
+
+	code := config.ZeroCode(pkg, "code")
+	code.AddDependency(config.NewCode(pkg, "foo", func(w io.Writer, c *Code) error {
+		fmt.Fprintln(w, "// with Code")
+		return nil
+	}))
+	code.AddDependency(&CodeEmitter{Code: config.NewCode(pkg, "bar", func(w io.Writer, c *Code) error {
+		fmt.Fprintln(w, "// with CodeEmitter")
+		return nil
+	})},
+	)
+
+	var buf bytes.Buffer
+	if err := (&CodeEmitter{Code: code}).Emit(&buf); err != nil {
+		t.Fatalf("unexpected error: %+v", err)
+	}
+
+	want := `
+package foo
+
+
+// with Code
+// with CodeEmitter
+`
+	if want, got := strings.TrimSpace(want), strings.TrimSpace(buf.String()); want != got {
+		difftest.LogDiffGotStringAndWantString(t, got, want)
+	}
+}
