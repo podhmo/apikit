c0ca2e3a24c8aaee7203fc51d04f89de176a6dad
podhmo <ababjam61+github@gmail.com>
2021-12-03 19:42:13 +0900

    feat: plugin with context.Context

--- plugins/plugin.go
+++ plugins/plugin.go
@@ -1,26 +1,47 @@
 package plugins
 
 import (
+	"context"
+	"fmt"
+	"reflect"
+
 	"github.com/podhmo/apikit/code"
 	"github.com/podhmo/apikit/pkg/emitgo"
 	"github.com/podhmo/apikit/pkg/tinypkg"
 	"github.com/podhmo/apikit/resolve"
 )
 
-func NewDefaultPluginContext(emitter *emitgo.Emitter) *PluginContext {
+func NewDefaultConfig(emitter *emitgo.Emitter) *PluginConfig {
 	config := code.DefaultConfig()
-	return &PluginContext{
+	return &PluginConfig{
 		Config:   config,
 		Emitter:  emitter,
 		Resolver: config.Resolver,
 	}
 }
 
-type PluginContext struct {
+type PluginConfig struct {
 	*code.Config
 
 	Emitter  *emitgo.Emitter
 	Resolver *resolve.Resolver
+}
+
+func (c *PluginConfig) ActivatePlugins(
+	ctx context.Context, here *tinypkg.Package,
+	plugins ...Plugin) error {
+	pc := &PluginContext{Context: ctx, PluginConfig: c}
+	for _, p := range plugins {
+		if err := pc.IncludePlugin(here, p); err != nil {
+			return fmt.Errorf("on plugin %v, %w", reflect.TypeOf(pc.IncludePlugin), err)
+		}
+	}
+	return nil
+}
+
+type PluginContext struct {
+	*PluginConfig
+	Context context.Context
 }
 
 func (pc *PluginContext) IncludePlugin(here *tinypkg.Package, p Plugin) error {
