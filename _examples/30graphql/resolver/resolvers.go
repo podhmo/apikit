463c6f72b05680a63c5b1057f8d4e30f75ab91d2
podhmo <ababjam61+github@gmail.com>
2021-12-06 22:01:20 +0900

    chore: type defs

--- _examples/30graphql/resolver/resolvers.go
+++ _examples/30graphql/resolver/resolvers.go
@@ -2,13 +2,24 @@ package resolver
 
 import "github.com/graphql-go/graphql"
 
+type Definitions struct {
+	dateType *graphql.Scalar
+	roleType *graphql.Enum
+
+	userType        *graphql.Object
+	chatType        *graphql.Object
+	chatMessageType *graphql.Object
+}
+
 func Schema() (graphql.Schema, error) {
+	var defs Definitions
+
 	// Args: graphql.FieldConfigArgument{
 	// 	"doneOnly": &graphql.ArgumentConfig{
 	// 		Type: graphql.Boolean,
 	// 	},
 	// },
-	roleType := graphql.NewEnum(graphql.EnumConfig{
+	defs.roleType = graphql.NewEnum(graphql.EnumConfig{
 		Name: "Role",
 		Values: graphql.EnumValueConfigMap{
 			"ADMIN": &graphql.EnumValueConfig{},
@@ -16,47 +27,72 @@ func Schema() (graphql.Schema, error) {
 		},
 	})
 
-	userType := graphql.NewObject(graphql.ObjectConfig{
+	defs.dateType = graphql.DateTime
+	defs.userType = graphql.NewObject(graphql.ObjectConfig{
 		Name: "User",
-		Fields: graphql.Fields{
-			"id": &graphql.Field{
-				Type: graphql.NewNonNull(graphql.ID),
-			},
-			"username": &graphql.Field{
-				Type: graphql.String,
-			},
-			"email": &graphql.Field{
-				Type: graphql.String,
-			},
-			"role": &graphql.Field{
-				Type: roleType,
-			},
-		},
+		Fields: (graphql.FieldsThunk)(func() graphql.Fields {
+			return graphql.Fields{
+				"id": &graphql.Field{
+					Type: graphql.NewNonNull(graphql.ID),
+				},
+				"username": &graphql.Field{
+					Type: graphql.String,
+				},
+				"email": &graphql.Field{
+					Type: graphql.String,
+				},
+				"role": &graphql.Field{
+					Type: defs.roleType,
+				},
+			}
+		}),
 	})
 
-	chatType := graphql.NewObject(graphql.ObjectConfig{
+	defs.chatType = graphql.NewObject(graphql.ObjectConfig{
 		Name: "Chat",
-		Fields: graphql.Fields{
-			"id": &graphql.Field{
-				Type: graphql.NewNonNull(graphql.ID),
-			},
-			"users": &graphql.Field{
-				Type: graphql.NewList(userType),
-			},
-			// "messages": &graphql.Fiel{
-			// 	Type: graphql.NewList(chatMessageType),
-			// },
-		},
+		Fields: (graphql.FieldsThunk)(func() graphql.Fields {
+			return graphql.Fields{
+				"id": &graphql.Field{
+					Type: graphql.NewNonNull(graphql.ID),
+				},
+				"users": &graphql.Field{
+					Type: graphql.NewList(defs.userType),
+				},
+				"messages": &graphql.Field{
+					Type: graphql.NewList(defs.chatMessageType),
+				},
+			}
+		}),
 		// Interfaces: (graphql.InterfacesThunk)(func() []*graphql.Interface {
 		// 	return []*graphql.Interface{someInterface}
 		// }),
+	})
+
+	defs.chatMessageType = graphql.NewObject(graphql.ObjectConfig{
+		Name: "chatMessage",
+		Fields: (graphql.FieldsThunk)(func() graphql.Fields {
+			return graphql.Fields{
+				"id": &graphql.Field{
+					Type: graphql.NewNonNull(graphql.ID),
+				},
+				"content": &graphql.Field{
+					Type: graphql.String,
+				},
+				"time": &graphql.Field{
+					Type: defs.dateType,
+				},
+				"user": &graphql.Field{
+					Type: defs.userType,
+				},
+			}
+		}),
 	})
 
 	query := graphql.NewObject(graphql.ObjectConfig{
 		Name: "query",
 		Fields: graphql.Fields{
 			"me": &graphql.Field{
-				Type:    userType,
+				Type:    defs.userType,
 				Resolve: ResolveMe,
 			},
 			"user": &graphql.Field{
@@ -65,15 +101,15 @@ func Schema() (graphql.Schema, error) {
 						Type: graphql.String,
 					},
 				},
-				Type:    userType,
+				Type:    defs.userType,
 				Resolve: ResolveUser,
 			},
 			"allUsers": &graphql.Field{
-				Type:    graphql.NewList(userType),
+				Type:    graphql.NewList(defs.userType),
 				Resolve: ResolveAllUsers,
 			},
 			"myChats": &graphql.Field{
-				Type:    graphql.NewList(chatType),
+				Type:    graphql.NewList(defs.chatType),
 				Resolve: ResolveMyChats,
 			},
 		},
