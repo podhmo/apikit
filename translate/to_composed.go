ecb3a3c338ee5aa82922da8ff559ebc7ea475f8b
podhmo <ababjam61+github@gmail.com>
2021-12-10 23:24:06 +0900

    fix: modify implementation

--- translate/to_composed.go
+++ translate/to_composed.go
@@ -3,7 +3,6 @@ package translate
 import (
 	"fmt"
 	"io"
-	"strings"
 
 	"github.com/podhmo/apikit/pkg/tinypkg"
 	"github.com/podhmo/apikit/resolve"
@@ -17,25 +16,72 @@ func writeComposed(
 	name string, providers []*resolve.Def,
 ) error {
 	// TODO: external
-	var args []*tinypkg.Var
-	var returns []*tinypkg.Var
-	symbols := map[reflectshape.Identity]*tinypkg.Symbol{}
+	type Var struct {
+		*tinypkg.Var
+		K reflectshape.Identity
+	}
+
+	vars := map[reflectshape.Identity]Var{}
 
 	for i, p := range providers {
-		k := p.Returns[0].Shape.GetIdentity()
-		symbols[k] = here.NewSymbol(fmt.Sprintf("v%d", i))
+		retItem := p.Returns[0].Shape
+		k := retItem.GetIdentity()
+		if _, ok := vars[k]; ok {
+			continue
+		}
+		name := fmt.Sprintf("v%d", i)
+		node := resolver.Symbol(here, retItem)
+		vars[k] = Var{K: k, Var: &tinypkg.Var{Name: name, Node: node}}
 	}
+
+	var args []*tinypkg.Var
 	for _, p := range providers {
 		for _, x := range p.Args {
 			k := x.Shape.GetIdentity()
-			_, ok := symbols[k]
+			v, ok := vars[k]
 			if !ok {
-				args = append(args, &tinypkg.Var{Name: x.Name, Node: resolver.Symbol(here, x.Shape)})
-				symbols[k] = here.NewSymbol(x.Name)
+				sym := resolver.Symbol(here, x.Shape)
+				args = append(args, &tinypkg.Var{Name: x.Name, Node: sym})
+				v = Var{K: k, Var: &tinypkg.Var{Name: x.Name, Node: sym}}
+				vars[k] = v
 			}
+			v.Name = x.Name
 		}
 	}
 
+	var bindings tinypkg.BindingList
+	for _, p := range providers {
+		retK := p.Returns[0].Shape.GetIdentity()
+		v := vars[retK]
+
+		sym := resolver.Symbol(here, p.Shape)
+		factory, ok := sym.(*tinypkg.Func)
+		if !ok {
+			// func() <component>
+			factory = here.NewFunc("", nil, []*tinypkg.Var{{Node: sym}})
+		}
+
+		b, err := tinypkg.NewBinding(v.Name, factory)
+		if err != nil {
+			return fmt.Errorf("on provider %s, new binding: %w", p.Symbol, err)
+		}
+		for i, x := range b.Provider.Args {
+			fmt.Println("@@", "arg", i, x, tinypkg.GetLevel(x.Node))
+			v := vars[p.Args[i].Shape.GetIdentity()]
+			fmt.Println("@#", "var", v.Name, v.Node, tinypkg.GetLevel(v.Node))
+		}
+		bindings = append(bindings, b)
+		fmt.Println("---")
+	}
+
+	bindings.Dump(args...)
+
+	sorted, err := bindings.TopologicalSorted(args...)
+	if err != nil {
+		return fmt.Errorf("topological sort: %w", err)
+	}
+
+	var returns []*tinypkg.Var
 	{
 		sym := resolver.Symbol(here, providers[len(providers)-1].Shape)
 		f, ok := sym.(*tinypkg.Func)
@@ -46,35 +92,15 @@ func writeComposed(
 	}
 	return tinypkg.WriteFunc(w, here, name, &tinypkg.Func{Args: args, Returns: returns},
 		func() error {
-			for i, p := range providers {
-				if i == len(providers)-1 {
-					args := make([]string, 0, len(p.Args))
-					for _, x := range p.Args {
-						k := x.Shape.GetIdentity()
-						v := symbols[k]
-						args = append(args, v.Name)
-					}
-					rhs := fmt.Sprintf("%s(%s)", tinypkg.ToRelativeTypeString(here, p.Symbol), strings.Join(args, ", "))
-					fmt.Fprintf(w, "\treturn %s\n", rhs)
+			indent := "\t"
+			for i, b := range sorted {
+				if i == len(sorted)-1 {
+					fmt.Fprintf(w, "%sreturn %s\n", indent, b.CallString(here))
 					break
 				}
 
-				retK := p.Returns[0].Shape.GetIdentity()
-				name := symbols[retK].Name
-
-				sym := resolver.Symbol(here, p.Shape)
-				factory, ok := sym.(*tinypkg.Func)
-				if !ok {
-					// func() <component>
-					factory = here.NewFunc("", nil, []*tinypkg.Var{{Node: sym}})
-				}
-				binding, err := tinypkg.NewBinding(name, factory)
-				if err != nil {
-					return fmt.Errorf("on provider %s, new binding: %w", p.Symbol, err)
-				}
-				indent := "\t"
-				if err := binding.WriteWithCleanupAndError(w, here, indent, returns); err != nil {
-					return fmt.Errorf("on provider %s, write binding: %w", p.Symbol, err)
+				if err := b.WriteWithCleanupAndError(w, here, indent, returns); err != nil {
+					return err
 				}
 			}
 			return nil
